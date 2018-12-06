2f52f89607f25356dddab0a9315220dc9dda7416
Alex Eshoo <AlexEshoo@gmail.com>
2018-12-06 09:18:00 -0500

    solved puzzle 3

--- puzzle03.py
+++ puzzle03.py
@@ -1,9 +1,32 @@
 import re
 
-def place_square(x, y, l, h):
-	print(x,y,l,h)
+def get_square_set(c_id, x, y, w, h):
+	area = []
 
-result = re.findall(r"#\d+ @ (\d+),(\d)+: (\d+)x(\d+)", "#1 @ 1,3: 4x4\n#2 @ 2,4: 10x321")
+	for i in range(w):
+		for j in range(h):
+			area.append((x + i, y + j))
 
-for r in result:
-	place_square(*r)
+	return c_id, set(area)
+
+
+with open("inputs/p03_input.txt") as f:
+	result = re.findall(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", f.read())
+
+sets = [get_square_set(*(int(i) for i in r)) for r in result]
+overlap = set()
+unique_plots = {s[0]: s[1] for s in sets}
+
+for i, s in enumerate(sets):
+	count = 0
+	the_set = s[1]
+	the_id = s[0]
+	for s2 in sets[i+1:]:
+		common = the_set.intersection(s2[1])
+		if len(common) > 0:
+			unique_plots.pop(the_id, None)
+			unique_plots.pop(s2[0], None)
+		overlap.update(common)
+
+print("Total Shared Area: ", len(overlap))
+print("The only unique claim is: ", list(unique_plots.keys())[0])
